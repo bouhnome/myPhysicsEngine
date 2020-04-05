6a1722288bf40588fc1b7de26295aac083725535
bouhnome <62355679+bouhnome@users.noreply.github.com>
2020-04-05 18:07:12 +0200

    Add files via upload

--- test.java
+++ test.java
@@ -1,63 +1,137 @@
 import java.util.*;
+import java.awt.Color;
 public class test{
-	public static void main(String [] args){
-		double t_init =0;//time of start of the simulation
-		double dt = 0.001;//time step
-		double t_fin  =100;//time of ending of the simulation
-		double n = (int)((t_fin-t_init)/dt)+1;//number of iterations of the simulation
-		
-
-
-
-
-		ParticleForceRegistry registry = new ParticleForceRegistry();//we declare the data structure that will hold the informations on the link between the particles and the force generators
-
-
-		//we declare all the force we might need
-		ParticleGravity poids = new ParticleGravity(new Apoint(0,0,-9.8));
-		ParticleGravity sidewaysPoids = new ParticleGravity(new Apoint(0,9.8,0));
-
-
-
-		//we declare a linkedlist for the particles
-		LinkedList<Particle> listOfParticles = new LinkedList<Particle>();
-
-
-
-		//the particle linked list if filled with some particles
-		listOfParticles.add(new Particle(new Apoint(0,0,10),0.01,0.5));
-		listOfParticles.add(new Particle(new Apoint(0,1,2),0.01,new Apoint(1,1,1)));
-		listOfParticles.add(new Particle(new Apoint(1,0,-10),0.01,new Apoint(0,9,0),0.3));
-		listOfParticles.add(new Particle(new Apoint(1,1,1),0.01));
-
+	
+	static ParticleForceRegistry registry = new ParticleForceRegistry();
+	static LinkedList<Particle> listOfParticles = new LinkedList<Particle>();
+	static LinkedList<Particle> structure = new LinkedList<Particle>();
+	static ParticleGravity poids = new ParticleGravity(new Apoint(0,4,0));
 
 		
 
-		//simulation loop
-		for(int i=1;i<=n;i++){
-			//we give weight to every particle from time t=0
-			if(i==1){
-				for(Particle b : listOfParticles)
-					registry.add(b,poids);
-			}
-			//we attach another force generator to the first particle in the list when i=500
-			if(i==500)
-				registry.add(listOfParticles.get(1),sidewaysPoids);
+	public static void simulate(double dt,int niter){
 
-			//the registry updates the forces of all the particles
-			registry.updateForces(dt);
-
-			//we update the velocity and position of all the particles
-			for(Particle b : listOfParticles)
-				b.integrate(dt);
-
-			//draw(); //we draw the frame 
-
-			//un print test pour voir si tout fonctionne bien
-			for(Particle b : listOfParticles)
-				System.out.println(b.position);
-			System.out.println();
-			System.out.println();
+		if(niter==350){
+			listOfParticles.get(0).velocity = Apoint.multByScalar(listOfParticles.get(0).velocity,70/7);
 		}
+boolean replenish = true;
+			for(int i =0;i<listOfParticles.size();i++){
+				if(listOfParticles.get(i).position.y<0.80*800-35)
+					replenish = false;
+			}
+
+			if(replenish){
+				int u = 0;
+		while(u<20)
+		{		listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*1-u*30,0),0.01,new Apoint(0,0,0),Color.white,30));
+				listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*3-30*(u),0),0.01,new Apoint(0,0,0),Color.orange,30));
+				listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*5-30*(u),0),0.01,new Apoint(0,0,0),Color.red,30));
+				listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*7-30*(u),0),0.01,new Apoint(0,0,0),Color.green,30));
+				listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*9-30*(u),0),0.01,new Apoint(0,0,0),Color.yellow,30));
+				
+				u = u+10;	
+		}			
+
+			}
+
+		
+			for(int j = 1;j<listOfParticles.size();j++)
+			{if(listOfParticles.get(0).position.x+listOfParticles.get(0).rayon>=listOfParticles.get(j).position.x-listOfParticles.get(j).rayon && listOfParticles.get(0).position.y+listOfParticles.get(0).rayon>=listOfParticles.get(j).position.y-listOfParticles.get(j).rayon && listOfParticles.get(0).position.y-listOfParticles.get(0).rayon<=listOfParticles.get(j).position.y+listOfParticles.get(j).rayon && listOfParticles.get(0).position.x-listOfParticles.get(0).rayon<=listOfParticles.get(j).position.x+listOfParticles.get(j).rayon){
+							double  a=listOfParticles.get(0).position.x+listOfParticles.get(0).rayon;
+							listOfParticles.get(0).position.x=listOfParticles.get(j).position.x-listOfParticles.get(j).rayon-listOfParticles.get(0).rayon;
+							listOfParticles.get(j).position.x=a+listOfParticles.get(j).rayon;
+							
+			
+							listOfParticles.get(0).velocity.x = -listOfParticles.get(0).velocity.x;
+							listOfParticles.get(j).velocity.x = -0.1*listOfParticles.get(0).velocity.x;
+			
+						}}
+
+			for(int i =1;i<listOfParticles.size();i++){
+				if(listOfParticles.get(i).velocity.x !=0 && (listOfParticles.get(i).position.x+listOfParticles.get(i).rayon<=1000-30 || listOfParticles.get(i).position.x-listOfParticles.get(i).rayon>=1000+30)){
+					registry.add(listOfParticles.get(i),poids);
+					listOfParticles.get(i).rebound = 0;
+				}
+
+				
+			}	//clear
+
+
+			for(int i =1 ;i<listOfParticles.size();i++){
+				for(int j =i+1;j<listOfParticles.size();j++){
+					if(listOfParticles.get(i).position.y>=0.80*800-1200 && listOfParticles.get(j).position.y>=0.80*800-40&& Apoint.magnitude(listOfParticles.get(j).velocity)!=0 && Apoint.magnitude(listOfParticles.get(i).velocity)!=0 ){
+					
+						if(listOfParticles.get(i).position.x+listOfParticles.get(i).rayon>=listOfParticles.get(j).position.x-listOfParticles.get(j).rayon && listOfParticles.get(i).position.y+listOfParticles.get(i).rayon>=listOfParticles.get(j).position.y-listOfParticles.get(j).rayon && listOfParticles.get(i).position.y-listOfParticles.get(i).rayon<=listOfParticles.get(j).position.y+listOfParticles.get(j).rayon && listOfParticles.get(i).position.x-listOfParticles.get(i).rayon<=listOfParticles.get(j).position.x+listOfParticles.get(j).rayon){
+							double  a=listOfParticles.get(i).position.x+listOfParticles.get(i).rayon;
+							listOfParticles.get(i).position.x=listOfParticles.get(j).position.x-listOfParticles.get(j).rayon-listOfParticles.get(i).rayon;
+							listOfParticles.get(j).position.x=a+listOfParticles.get(j).rayon;
+							
+								listOfParticles.get(i).myColor = Color.black;
+								listOfParticles.get(j).myColor = Color.black;
+								listOfParticles.get(i).damping = 0.95;
+								listOfParticles.get(j).damping = 0.95;
+							listOfParticles.get(i).velocity.x = -listOfParticles.get(i).velocity.x;
+							listOfParticles.get(j).velocity.x = -listOfParticles.get(i).velocity.x;
+			
+						}
+					}
+				}
+
+			}	
+
+			for(int i = 1; i<listOfParticles.size();i++)	
+{			if(listOfParticles.get(i).position.y>=0.80*800-40 && Apoint.magnitude(listOfParticles.get(i).velocity)!=0 && (listOfParticles.get(i).position.x+listOfParticles.get(i).rayon>=1000-30 &&listOfParticles.get(i).position.x-listOfParticles.get(i).rayon<=1000+30 )){
+				if(listOfParticles.get(i).position.x+listOfParticles.get(i).rayon>=1000-30 ){
+						listOfParticles.get(i).position.x=1000-30-listOfParticles.get(i).rayon;
+						listOfParticles.get(i).velocity.x=-30;
+				}
+	
+			}}
+		
+		
+		if(listOfParticles.get(0).position.x+listOfParticles.get(0).rayon>=1000-30&&(listOfParticles.get(0).position.x==1000000)){
+			listOfParticles.get(0).position.x=1000-30-listOfParticles.get(0).rayon;
+			listOfParticles.get(0).velocity.x=-listOfParticles.get(0).velocity.x;
+			listOfParticles.get(5).velocity.x=40;
+			listOfParticles.get(0).rebound=0.2;
+			listOfParticles.get(5).rebound=0.2;
+			registry.add(listOfParticles.get(0),poids);
+			registry.add(listOfParticles.get(5),poids);
+		}
+		if(listOfParticles.get(5).position.x+listOfParticles.get(5).rayon>=1000+60&&(listOfParticles.get(0).position.x==-1000000)){
+			for(int i = 6;i<listOfParticles.size();i++){
+				registry.add(listOfParticles.get(i),poids);
+				listOfParticles.get(i).rebound = 0.5;
+			}
+		}
+		
+if(listOfParticles.size()>=29){
+	registry.clear();
+	
+}
+
+		
+
+		registry.updateForces(dt);
+		for(Particle b : listOfParticles)
+			b.integrate(dt);
+	}
+
+	public static void main(String [] args){
+		listOfParticles.add(new Particle(new Apoint(0,0.8*800,0),0.01,Apoint.multByScalar(Apoint.normalize(new Apoint(1000,0.8*800-30,0)),7),Color.black,6));
+
+		int u = 0;
+		while(u<20)
+		{		listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*1-u*30,0),0.01,new Apoint(0,0,0),Color.white,30));
+				listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*3-30*(u),0),0.01,new Apoint(0,0,0),Color.orange,30));
+				listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*5-30*(u),0),0.01,new Apoint(0,0,0),Color.red,30));
+				listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*7-30*(u),0),0.01,new Apoint(0,0,0),Color.green,30));
+				listOfParticles.add(new Particle(new Apoint(1000,800*0.80-30*9-30*(u),0),0.01,new Apoint(0,0,0),Color.yellow,30));
+				
+				u = u+10;	
+		}			
+
+		FenetrePlotCourbe fenetre = new FenetrePlotCourbe(listOfParticles);
+		fenetre.lancement();
 	}
 }
